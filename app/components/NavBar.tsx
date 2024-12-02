401d8e2cb839b9a0be8d6f6b51247234c18303ac
lutfiEmre <you@example.com>
2024-12-02 13:50:19 +0300

    responsive fixes and some update

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,8 +2,11 @@
 import React, { useEffect, useState } from 'react';
 
 import Image from "next/image";
-import logosvg from '../../public/logo.svg'
+import leftlogo from '@/public/leftlogo.svg'
+import rightlogo from '@/public/rightlogo.svg'
 import Link from "next/link";
+
+import { motion } from "framer-motion";
 
 
 const NavBar = () => {
@@ -34,9 +37,27 @@ const NavBar = () => {
             className={`flex flex-row sticky ${
                 showBg ? 'bg-violet3' : 'bg-transparent'
             } pb-4 top-0 left-0 w-full justify-center z-30 items-center lg:items-start lg:justify-start`}
-        >            <div className={'pt-[40px] flex md:flex-col z-50 w-fit lg:w-full flex-col gap-[50px] lg:flex-row justify-between items-center mx-[25px]'}>
-                <Image width={317} height={43} src={logosvg} alt={'activ8earth'}/>
-                <div className={'grid grid-cols-2 xl:grid-cols-4  sm:flex flex-wrap sm:flex-nowrap justify-center flex-row gap-[48px] items-center'}>
+        >            <div className={'flex md:flex-col  z-50 pt-[30px] px-4 sm:pt-[20px]  w-fit lg:w-full flex-col gap-[20px] sm:gap-[50px] lg:flex-row justify-between items-center mx-[25px]'}>
+            <div className={'flex flex-row gap-1'}>
+                <motion.div
+                    animate={{
+                        rotate: [0, 90, 180, 270, 360], // 90 derece aralıklarla döndürme
+                        scale: [1, 1.1, 1], // Dönerken hafifçe büyüme
+
+                    }}
+                    transition={{
+                        duration: 2, // Döngünün süresi
+                        repeat: Infinity, // Sonsuz döngü
+                        ease: "linear", // Sabit hız
+                    }}
+
+                >
+                    <Image style={{ filter: "drop-shadow(0px 0px 20px white)" }} className="" width={69} height={43} src={leftlogo} alt="activ8earth" />
+                </motion.div>
+                <Image style={{ filter: "drop-shadow(0px 0px 4px white)" }} className={''} width={252} height={43} src={rightlogo} alt={'activ8earth'}/>
+
+            </div>
+                <div className={'grid grid-cols-2 xl:grid-cols-4  sm:flex flex-wrap sm:flex-nowrap justify-center justify-items-center flex-row gap-[20px] sm:gap-[48px] items-center'}>
 
                     <Link href={'#about'}>
                         <h6 className={'rubik-medium text-[20px] text-white'}>
@@ -61,8 +82,8 @@ const NavBar = () => {
                     </Link>
 
                 </div>
-                <div className={'px-[33px]  cursor-pointer w-full text-center lg:w-fit py-[16px] bg1'}>
-                    <h6 className={'rubik-medium text-[16px] text-white'}>
+                <div className={`${showBg && "sm:hidden"} lg:!flex sm:px-[33px] px-[24px]  cursor-pointer w-full text-center lg:w-fit py-[12px] sm:py-[16px] bg1`}>
+                    <h6 className={'rubik-medium text-[14px] sm:text-[16px] text-white'}>
                         Entire The World!
                     </h6>
                 </div>
