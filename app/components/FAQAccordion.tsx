cb42fe5b180fd8028734f176e3957216c8fb3a0e
lutfiEmre <you@example.com>
2024-12-05 22:28:18 +0300

    asd

--- app/components/FAQAccordion.tsx
+++ app/components/FAQAccordion.tsx
@@ -43,24 +43,45 @@ const FAQAccordion: React.FC = () => {
             <div className="mx-2 my-4 w-full md:w-full">
                 {/* `transitionTimeout` prop should be equal to the transition duration in CSS */}
                 <Accordion transition transitionTimeout={200}>
-                    <AccordionItem header="Q1: What is NFTWeb?">
-                        Lorem ipsum dolor sit amet consectetur. Convallis cras placerat dignissim aliquam massa. Aliquet volutpat rhoncus in convallis consectetur. Cras adipiscing volutpat non hac enim odio enim.
+                    <AccordionItem header="Q1: What can be done in the dApp?">
+                        Individuals or brands create activity/advertisement. Users can earn by completing these activities.
                     </AccordionItem>
 
-                    <AccordionItem header="Q2: How does NFTWeb work?">
-                        Lorem ipsum dolor sit amet consectetur. Convallis cras placerat dignissim aliquam massa. Aliquet volutpat rhoncus in convallis consectetur. Cras adipiscing volutpat non hac enim odio enim.
+                    <AccordionItem header="Q2: How can people benefit from this dApp?">
+                        Users who create activities/ads within the app keep the ecosystem alive and make profits with
+                        monthly token rewards. Users who complete these created activities can share the created reward
+                        pools and earn income.
                     </AccordionItem>
 
-                    <AccordionItem header="Q3: What are the main features of NFT NFTWeb?">
-                        Lorem ipsum dolor sit amet consectetur. Convallis cras placerat dignissim aliquam massa. Aliquet volutpat rhoncus in convallis consectetur. Cras adipiscing volutpat non hac enim odio enim.
+                    <AccordionItem header="Q3: Who can create activities/ads in the dApp?">
+                        Activator Gem NFT owners can create activities.
                     </AccordionItem>
 
-                    <AccordionItem header="Q4: How can I acquire NFTWeb?">
-                        Lorem ipsum dolor sit amet consectetur. Convallis cras placerat dignissim aliquam massa. Aliquet volutpat rhoncus in convallis consectetur. Cras adipiscing volutpat non hac enim odio enim.
+                    <AccordionItem header="Q4: How can Activate Earth users participate in activities?">
+                        They can participate by holding/staking the Activate Earths own tokens in their web3 wallet.
                     </AccordionItem>
 
-                    <AccordionItem header="Q5: Is NFT XYCoin open to artists and creators?">
-                        Lorem ipsum dolor sit amet consectetur. Convallis cras placerat dignissim aliquam massa. Aliquet volutpat rhoncus in convallis consectetur. Cras adipiscing volutpat non hac enim odio enim.
+                    <AccordionItem header="Q5: What are the advantages of NFTs?">
+                        Each piece in the Activator Gem NFT Collection will be able to create an in-dApp activity/reward pool
+                        and will be able to benefit from creator rewards to be distributed within the dApp. In addition,
+                        whitelist priority will be provided in the airdrop event.
+                    </AccordionItem>
+                    <AccordionItem header="Q6: What is the reward program that dApp offers to its users?">
+                        It offers activity creators the opportunity to earn and improve their activities/ads with monthly token
+                        rewards that will be distributed systematically. Activity participants can share the reward pools of the
+                        activities they complete and earn for different experiences.
+
+                    </AccordionItem>
+                    <AccordionItem header="Q7: Are there any advertising or sponsorship opportunities on the Activate Earth platform?">
+                        Since the way the dApp works is different from other platforms, it will be experience-based
+                        advertising work. Yes, welcome to a platform where such opportunities are unlimited.
+
+                    </AccordionItem>
+                    <AccordionItem header="Q8: What are the processes for signing a partnership with the platform?">
+                        Activate Earth always aims to continue its existence as an application for the benefit of the brands-
+                        users-community. In this direction, processes may vary in order to determine priorities.
+
+
                     </AccordionItem>
                 </Accordion>
             </div>
