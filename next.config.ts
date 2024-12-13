580143a6f4b17445c40fac397497e62249c57168
”sinanartun” <26043673+sinanartun@users.noreply.github.com>
2024-12-13 02:20:26 +0100

    Add headers configuration and enable React strict mode in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    // Diğer yapılandırma ayarları (örneğin, reactStrictMode gibi)
+    reactStrictMode: true,
+
+    // Başlıkları ayarlamak için headers fonksiyonu
+    async headers() {
+        return [
+            {
+                source: "/(.*)", // Tüm yollar için bu başlıklar geçerli olacak
+                headers: [
+                    {
+                        key: "Cross-Origin-Opener-Policy",
+                        value: "same-origin",
+                    },
+                    {
+                        key: "Cross-Origin-Embedder-Policy",
+                        value: "require-corp",
+                    },
+                ],
+            },
+        ];
+    },
+    output: 'export',
 };
 
 export default nextConfig;
